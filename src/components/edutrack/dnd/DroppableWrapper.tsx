9f664610ff5766496a3ca6bd6506d0336bf90c83
Tanvir Mahmud <learntanvir@gmail.com>
2025-09-24 00:42:33 +0000

    Try fixing this error: `Console Error: [[react-beautiful-dnd ]A setup pr

--- src/components/edutrack/dnd/DroppableWrapper.tsx
+++ src/components/edutrack/dnd/DroppableWrapper.tsx
@@ -5,20 +5,10 @@ import { useEffect, useState } from "react";
 import { Droppable, DroppableProps } from "react-beautiful-dnd";
 
 export const DroppableWrapper = ({ children, ...props }: DroppableProps) => {
-  const [enabled, setEnabled] = useState(false);
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
-  useEffect(() => {
-    const animation = requestAnimationFrame(() => setEnabled(true));
-
-    return () => {
-      cancelAnimationFrame(animation);
-      setEnabled(false);
-    };
-  }, []);
-
-  if (!enabled) {
-    return null;
-  }
-
-  return <Droppable {...props}>{children}</Droppable>;
+    return isClient ? <Droppable {...props}>{children}</Droppable> : null;
 };
