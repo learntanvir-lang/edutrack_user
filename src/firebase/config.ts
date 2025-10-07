5be0cef9ace39469b51c9a6c7225b0a4b0564ebf
Tanvir Mahmud <learntanvir@gmail.com>
2025-10-07 15:30:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "edutrackuser-68119702-f967f",
+  "appId": "1:46282668021:web:db21ff45b3d173e84fd2bc",
+  "apiKey": "AIzaSyDEVWIYUdKkVww7hI_TecGO6jyfI0rHSiM",
+  "authDomain": "edutrackuser-68119702-f967f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "46282668021"
+};
