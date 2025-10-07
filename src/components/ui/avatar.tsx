cb202249bf61e98a0391dfbf1c9718cb1e1f7e05
Tanvir Mahmud <learntanvir@gmail.com>
2025-10-07 15:56:19 +0000

    add circle border of main volor (_for element <Primitive.span>_)

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -12,7 +13,7 @@ const Avatar = React.forwardRef<
   <AvatarPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full border-2 border-primary/80",
       className
     )}
     {...props}
