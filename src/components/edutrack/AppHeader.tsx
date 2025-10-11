1222254552019aab03554e8228fda9bfbf76a1b2
Tanvir Mahmud <learntanvir@gmail.com>
2025-10-11 11:16:10 +0000

    instead of notes add here Resources icon with Resources text (_for eleme

--- src/components/edutrack/AppHeader.tsx
+++ src/components/edutrack/AppHeader.tsx
@@ -16,7 +16,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { signOut } from 'firebase/auth';
-import { LogOut, Sparkles, KeyRound, Notebook, User as UserIcon, LayoutDashboard, BookCopy, Target, ListTodo, LineChart } from 'lucide-react';
+import { LogOut, Sparkles, KeyRound, User as UserIcon, LayoutDashboard, BookCopy, Target, ListTodo, Library } from 'lucide-react';
 import { usePathname, useRouter } from 'next/navigation';
 import { Skeleton } from '../ui/skeleton';
 import { ChangePasswordDialog } from './ChangePasswordDialog';
@@ -54,7 +54,7 @@ export function AppHeader() {
       { href: '/syllabus', label: 'Syllabus', icon: BookCopy },
       { href: '/exams', label: 'Exams', icon: Target },
       { href: '/studytask', label: 'Study Tasks', icon: ListTodo },
-      { href: '/notes', label: 'Notes', icon: Notebook },
+      { href: '/notes', label: 'Resources', icon: Library },
   ];
 
   return (
